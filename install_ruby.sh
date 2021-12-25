70ddc96438cca31b69a50961ffc29cc9b2318d37
Adrian Lamo <80713541+gsb-eth0@users.noreply.github.com>
2021-12-25 16:14:21 +0300

    Update install_ruby.sh

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get -y install -y ruby-full ruby-bundler build-essential apt-transport-https ca-certificates
-bundler -v
+sudo apt-get update
+sudo apt upgrade
+sudo apt install -y ruby-full ruby-bundler build-essential apt-transport-https ca-certificates
